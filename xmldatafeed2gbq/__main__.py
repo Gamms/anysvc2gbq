8e403d5793afe33eb23fcc3531fea843ec0c074b
gamms <gamms@mail.ru>
2021-11-20 20:43:48 +0300

    Добавим YM

--- xmldatafeed2gbq/__main__.py
+++ xmldatafeed2gbq/__main__.py
@@ -1,31 +1,163 @@
 # type: ignore[attr-defined]
 from typing import Optional
 
+import csv
+import datetime
+import os
+import shutil
+import xml.etree.ElementTree as ET
+import zipfile
 from enum import Enum
 from random import choice
 
+import bq_method
+import requests
 import typer
+from loguru import logger
 from rich.console import Console
 
 from xmldatafeed2gbq import version
 from xmldatafeed2gbq.example import hello
 
-
-class Color(str, Enum):
-    white = "white"
-    red = "red"
-    cyan = "cyan"
-    magenta = "magenta"
-    yellow = "yellow"
-    green = "green"
-
-
+folderUnzipped = "unzipped/"
+folderUnzippedError = "unzippedERROR/"
+folderComplete = "complete/"
+folderDownloaded = "downloaded/"
+folderDownloadedError = "downloadedERROR/"
+folderError = "error/"
+folderLog = "log/"
 app = typer.Typer(
     name="xmldatafeed2gbq",
     help="import from xmldatafeed.com to Google big query",
     add_completion=False,
 )
 console = Console()
+
+
+def returnContentsListByType(typefile: str):
+    if typefile == "ozon":
+        list = [
+            "Date",
+            "Brend",
+            "Seller",
+            "Name_pr",
+            "Code",
+            "Position",
+            "Page_n",
+            "Old_pr",
+            "Price",
+        ]
+    elif typefile == "ozon_char":
+        list = [
+            "Date",
+            "Brend",
+            "Seller",
+            "Name_pr",
+            "Code",
+            "Sup_art",
+            "Position",
+            "Page_n",
+            "Old_pr",
+            "Price",
+            "Size",
+            "Type",
+            "Cover_fb",
+            "Form",
+            "Pattern",
+            "Color",
+        ]
+    elif typefile == "wb":
+        list = [
+            "Date",
+            "Brend",
+            "Seller",
+            "Name_pr",
+            "Code",
+            "Position",
+            "Page_n",
+            "Old_pr",
+            "Price",
+            "Cover_fb",
+            "Color",
+            "Discount",
+            "Rating",
+            "Reviwes",
+        ]
+    elif typefile == "ym":
+        list = [
+            "Date",
+            "Brend",
+            "Name_pr",
+            "Code",
+            "Position",
+            "Page_n",
+            "Old_pr",
+            "Price",
+            "Shipping",
+        ]
+
+    else:
+        list = []
+    return list
+
+
+def getallfilesfromxmlfeed(session, url, path, path_for_download, local_file_list):
+    res = session.request("PROPFIND", url + path)
+    root = ET.fromstring(res.content)
+    elements = root.findall("{DAV:}response/{DAV:}href")
+    for element in elements:
+        if element.text == path:
+            continue
+        pathlocal = element.text
+        if pathlocal.find("csv.zip") > -1:
+            filename = os.path.basename(pathlocal)
+            if filename in local_file_list:
+                continue
+            resfile = session.get(url + pathlocal)
+            if resfile.status_code == 200:
+                img = resfile.content
+                print(path_for_download + filename)
+
+                with open(path_for_download + filename, "wb") as f:
+                    f.write(img)
+
+        else:
+            getallfilesfromxmlfeed(
+                session, url, pathlocal, path_for_download, local_file_list
+            )
+
+
+def unzipFile(filename, path_for_download):
+    with zipfile.ZipFile(path_for_download + filename, "r") as zip_ref:
+        for fileinzip in zip_ref.filelist:
+            fileinzipname = fileinzip.filename
+            zip_ref.extract(fileinzipname, path_for_download + folderUnzipped)
+
+
+def readCsv2Js(fileinzipname, typefile, path_for_download):
+    jslist = []
+    with open(path_for_download + fileinzipname) as csvfile:
+        reader = csv.reader(csvfile, delimiter=";")
+        rowcount = 0
+        for row in reader:
+            rowcount += 1
+            if rowcount <= 2:
+                continue
+            dict = {}
+            contentlist = returnContentsListByType(typefile)
+
+            for index, value in enumerate(contentlist):
+                if value == "Date":
+                    dict[value] = (
+                        datetime.datetime.strptime(row[index], "%d.%m.%Y")
+                        .date()
+                        .isoformat()
+                    )
+                else:
+                    dict[value] = row[index]
+
+            jslist.append(dict)
+    return jslist
 
 
 def version_callback(print_version: bool) -> None:
@@ -35,32 +167,102 @@ def version_callback(print_version: bool) -> None:
         raise typer.Exit()
 
 
-@app.command(name="")
-def main(
-    name: str = typer.Option(..., help="Person to greet."),
-    color: Optional[Color] = typer.Option(
-        None,
-        "-c",
-        "--color",
-        "--colour",
-        case_sensitive=False,
-        help="Color for print. If not specified then choice will be random.",
-    ),
-    print_version: bool = typer.Option(
-        None,
-        "-v",
-        "--version",
-        callback=version_callback,
-        is_eager=True,
-        help="Prints the version of the xmldatafeed2gbq package.",
-    ),
-) -> None:
-    """Print a greeting with a giving name."""
-    if color is None:
-        color = choice(list(Color))
+def get_list_local_file(downloadpath, list_folder):
+    result = []
+    for el in list_folder:
+        content = os.listdir(downloadpath + el)
+        for file in content:
+            if os.path.isfile(os.path.join(downloadpath + el, file)) and file.endswith(
+                ".zip"
+            ):
+                result.append(file)
+    return result
 
-    greeting: str = hello(name)
-    console.print(f"[bold {color}]{greeting}[/]")
+
+@logger.catch
+@app.command()
+def main(
+    downloadpath: str, user: str, password: str, bqjsonservicefile: str, bqdataset: str
+) -> None:
+    logger.add(
+        downloadpath + folderLog + "infolog_{time}.log",
+        rotation="12:00",
+        compression=zip,
+        retention="14 days",
+        level="INFO",
+    )
+    logger.add(
+        downloadpath + folderLog + "errorlog_{time}.log",
+        rotation="100 MB",
+        compression=zip,
+        retention="100 days",
+        level="ERROR",
+    )
+    auth = requests.auth.HTTPBasicAuth(user, password)
+    basicurl = "https://ru.xmldatafeed.com"
+    basicpath = "/remote.php/dav/files/ankuznet@gmail.com/"
+    session = requests.Session()
+    session.auth = auth
+    list_local_file = get_list_local_file(
+        downloadpath, [folderDownloaded, folderDownloadedError]
+    )
+
+    getallfilesfromxmlfeed(session, basicurl, basicpath, downloadpath, list_local_file)
+
+    unzipFileInFolder(downloadpath)
+
+    content = os.listdir(downloadpath + folderUnzipped)
+    for file in content:
+        if os.path.isfile(
+            os.path.join(downloadpath + folderUnzipped, file)
+        ) and file.endswith(".csv"):
+            typefile = ""
+            if file.find("wildberries") > -1:
+                typefile = "wb"
+                bqtableid = "WB_pars_auto"
+            elif file.find("ozonbagchaircategories_characteristics") > -1:
+                typefile = "ozon_char"
+                bqtableid = "Ozon_pars_auto"
+            elif file.find("ozon") > -1:
+                typefile = "ozon"
+                bqtableid = "Ozon_pars_auto"
+            elif file.find("yamarket") > -1:
+                typefile = "ym"
+                bqtableid = "Ym_pars_auto"
+            else:
+                typefile = "error"
+                logger.error(f"Неизвестный тип файла:{file}")
+                return
+            try:
+                logger.info(f"Читаем {file}")
+                jslist = readCsv2Js(file, typefile, downloadpath + folderUnzipped)
+                bq_method.export_js_to_bq(
+                    jslist, bqtableid, bqjsonservicefile, bqdataset, logger
+                )
+                shutil.move(
+                    downloadpath + folderUnzipped + file,
+                    downloadpath + folderComplete + file,
+                )
+            except Exception as e:
+                logger.exception("Ошибка чтения csv")
+                shutil.move(
+                    downloadpath + folderUnzipped + file,
+                    downloadpath + folderError + file,
+                )
+
+
+def unzipFileInFolder(downloadpath):
+    content = os.listdir(downloadpath)
+    for file in content:
+        if os.path.isfile(os.path.join(downloadpath, file)) and file.endswith(".zip"):
+            try:
+                unzipFile(file, downloadpath)
+                shutil.move(downloadpath + file, downloadpath + folderDownloaded + file)
+            except Exception as e:
+                logger.exception("Ошибка распаковки")
+                shutil.move(
+                    downloadpath + file, downloadpath + folderDownloadedError + file
+                )
 
 
 if __name__ == "__main__":
---
+++ xmldatafeed2gbq/bq_method.py
@@ -0,0 +1,163 @@
+import datetime
+import os
+
+import loguru
+import pytz
+from google.cloud import bigquery as bq
+from google.oauth2 import service_account
+
+
+def export_js_to_bq(js, tableid, key_path, dataset_id, logger):
+    table_id = tableid
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
+    credentials = service_account.Credentials.from_service_account_file(
+        key_path,
+        scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    )
+    bigquery_client = bq.Client()
+    dataset_ref = bigquery_client.dataset(dataset_id)
+    job_config = bq.LoadJobConfig()
+    # Schema autodetection enabled
+    job_config.autodetect = True
+    # Skipping first row which correspnds to the field names
+    # Format of the data in GCS
+    job_config.source_format = bq.SourceFormat.NEWLINE_DELIMITED_JSON
+    job_config.schema_update_options = bq.SchemaUpdateOption.ALLOW_FIELD_ADDITION
+
+    dataset_ref = bigquery_client.dataset(dataset_id)
+    table_ref = dataset_ref.table(table_id)
+
+    logger.info(
+        f"Starting uploading data. Record count {len(js)} into the BQ table {table_id}"
+    )
+    job = bigquery_client.load_table_from_json(js, table_ref, job_config=job_config)
+    # load_job = bigquery_client.load_table_from_file(csvfile,table_ref,job_config)
+    try:
+        job.result()  # Waits for table load to complete.
+        logger.info(f"end uploading data.")
+    except Exception as e:
+        print("Unexpected error:", e)
+        raise
+
+
+def CreateDataSet(dataset_id, key_path):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
+    credentials = service_account.Credentials.from_service_account_file(
+        key_path,
+        scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    )
+
+    bigquery_client = bq.Client()
+    dataset = bq.Dataset(f"{credentials.project_id}.{dataset_id}")
+
+    # Send the dataset to the API for creation.
+    # Raises google.api_core.exceptions.Conflict if the Dataset already
+    # exists within the project.
+    dataset = bigquery_client.create_dataset(dataset)  # Make an API request.
+    print(f"Created dataset {bigquery_client.project}.{dataset.dataset_id}")
+
+
+def DeleteTable(table_id, dataset_id, key_path):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
+    credentials = service_account.Credentials.from_service_account_file(
+        key_path,
+        scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    )
+
+    bigquery_client = bq.Client()
+    # dataset = bq.Dataset('{}.{}'.format(credentials.project_id,dataset_id))
+    fulltableid = f"{credentials.project_id}.{dataset_id}.{table_id}"
+    # Send the dataset to the API for creation.
+    # Raises google.api_core.exceptions.Conflict if the Dataset already
+    # exists within the project.
+    dataset = bigquery_client.delete_table(fulltableid)  # Make an API request.
+    print(f"Delete table {fulltableid}")
+
+
+def GetMaxRecord(table_id, dataset_id, key_path, wb_id, field):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
+    credentials = service_account.Credentials.from_service_account_file(
+        key_path,
+        scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    )
+    fulltableid = f"{credentials.project_id}.{dataset_id}.{table_id}"
+    bigquery_client = bq.Client()
+    try:
+
+        query = f'SELECT Max(`{field}`) as MaxlastChangeDate  FROM `{fulltableid}` where wb_id="{wb_id}"'
+        job_query = bigquery_client.query(query, project=credentials.project_id)
+        results = job_query.result()
+        maxdate = datetime.date(1, 1, 1)
+        for row in results:
+            maxdate = row.MaxlastChangeDate
+            break
+    except Exception as e:
+        maxdate = datetime.datetime(1, 1, 1)
+    if maxdate == None:
+        maxdate = datetime.datetime(1, 1, 1)
+    maxdate = maxdate.replace(tzinfo=pytz.UTC)
+    return maxdate
+
+
+def DeleteOldReport(datefrom, dateto, dataset_id, key_path, fieldname, table_id, wb_id):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
+    credentials = service_account.Credentials.from_service_account_file(
+        key_path,
+        scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    )
+    fulltableid = f"{credentials.project_id}.{dataset_id}.{table_id}"
+    bigquery_client = bq.Client()
+    try:
+        query = f'Delete FROM `{fulltableid}` where {fieldname}>="{datefrom.strftime("%Y-%m-%d")}" and {fieldname} <"{dateto.strftime("%Y-%m-%d")}" and wb_id="{wb_id}"'
+        job_query = bigquery_client.query(query, project=credentials.project_id)
+        results = job_query.result()
+    except Exception as e:
+        print(e)
+    return
+
+
+def SelectQuery(key_path, dataset_id, table_id):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
+    credentials = service_account.Credentials.from_service_account_file(
+        key_path,
+        scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    )
+    fulltableid = f"{credentials.project_id}.{dataset_id}.{table_id}"
+    bigquery_client = bq.Client()
+    try:
+        query = f"Select * FROM `{fulltableid}` limit 100"
+        job_query = bigquery_client.query(query, project=credentials.project_id)
+        results = job_query.result()
+    except Exception as e:
+        print(e)
+    return results
+
+
+def DeleteRowFromTable(table_id, dataset_id, key_path, filtersList: list):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
+    credentials = service_account.Credentials.from_service_account_file(
+        key_path,
+        scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    )
+    fulltableid = f"{credentials.project_id}.{dataset_id}.{table_id}"
+    bigquery_client = bq.Client()
+    results = 0
+    try:
+        query = f"Delete FROM `{fulltableid}` where True"
+        for elFilter in filtersList:
+            if type(elFilter["value"]) is str:
+                query = (
+                    query
+                    + f' and {elFilter["fieldname"]}{elFilter["operator"]}"{elFilter["value"]}"'
+                )
+            else:
+                query = (
+                    query
+                    + f" and {elFilter['fieldname']}{elFilter['operator']}{elFilter['value']}"
+                )
+
+        job_query = bigquery_client.query(query, project=credentials.project_id)
+        results = job_query.result()
+    except Exception as e:
+        print(e)
+    return results
